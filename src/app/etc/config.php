c08e2a96d2ff8c504ef8503994c31a3775f27c04
juandacampoc <juandavidcampo@outlook.com>
2022-08-27 16:01:46 -0500

    agregue estilos del conflicto anterior

--- src/app/etc/config.php
+++ src/app/etc/config.php
@@ -324,7 +324,8 @@ return [
         'Magento_Wishlist' => 1,
         'Magento_WishlistAnalytics' => 1,
         'Magento_WishlistGraphQl' => 1,
-        'MercadoPago_Core' => 1,
+        'AgSofware_Checkout' => 1,
+        'AgSofware_Vendor' => 1,
         'PayPal_Braintree' => 1,
         'PayPal_BraintreeGraphQl' => 1,
         'Temando_ShippingRemover' => 1
