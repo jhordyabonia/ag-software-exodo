1be8885d5832e8c4a126e2765d5139873994f713
Angel Coyasos <angelcotr@gmail.com>
2022-09-14 14:37:48 -0500

    customer_social-login-dvlp: funcinalidad boton facebook terminada y lista para pruebas

---
+++ src/app/design/frontend/AgSoftware/base/web/js/customer/login-with-google.js
@@ -0,0 +1,25 @@
+
+//Iniciar seesion de usuarios
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    var id_token = googleUser.getAuthResponse().id_token;
+    console.log(id_token);
+}
+
+
+//Terminar seesion de usuario
+  function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+}
+
+//Funcion para disparar onSignIn
+function disp() {
+  onSignIn();
+}
