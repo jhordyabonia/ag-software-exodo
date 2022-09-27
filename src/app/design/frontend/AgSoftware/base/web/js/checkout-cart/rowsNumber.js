52c0a95a201f8b72e920f2e6150ab40538a77e0b
Angel Coyasos <angelcotr@gmail.com>
2022-09-27 16:30:34 -0500

    revision-general-pagina: mejora funcion botones y stylos de los mismo

--- src/app/design/frontend/AgSoftware/base/web/js/checkout-cart/rowsNumber.js
+++ src/app/design/frontend/AgSoftware/base/web/js/checkout-cart/rowsNumber.js
@@ -1,22 +1,27 @@
 //let input = document.getElementById('cart-35-qty');
 let input = document.querySelector('input[data-role="cart-item-qty"]');
-let disableBtn = document.getElementById('decreaseBtn');
+let decreaseBtn = document.getElementById('decreaseBtn');
+let increaseBtn = document.getElementById('increaseBtn');
 
-function counterNumber(valuePar) {
+decreaseBtn.addEventListener('click', decreaseNumber, true);
+increaseBtn.addEventListener('click', increaseNumber, true);
+
+
+function increaseNumber(valuePar) {
 
     let startValue = input.value;
 
-    if(valuePar.value == 'increase') {
-        startValue++;
-    } else {
-        startValue--;
-    }
+    startValue++;
 
-    if(startValue == 0) {
-        disableBtn.disabled = true;
-    } else {
-        disableBtn.disabled = false;
-    }
+    input.value = startValue;
+    
+}
+
+function decreaseNumber(valuePar) {
+
+    let startValue = input.value;
+
+    startValue--;
 
     input.value = startValue;
     
