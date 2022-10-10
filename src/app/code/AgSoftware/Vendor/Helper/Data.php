8019a474a06faa541872b8c80f797dcafc89bcfc
michael campo <michaelelcavo@gmail.com>
2022-10-10 14:52:28 -0500

    archivo helper

---
+++ src/app/code/AgSoftware/Vendor/Helper/Data.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace AgSoftware\Vendor\Helper;
+
+use Magento\Customer\Api\CustomerRepositoryInterface;
+use Magento\Framework\Pricing\PriceCurrencyInterface;
+use Magento\Quote\Model\Quote\Item\AbstractItem;
+use Magento\Store\Model\Store;
+use Magento\Store\Model\ScopeInterface;
+use Magento\Sales\Api\PaymentFailuresInterface;
+
+/**
+ * Checkout default helper
+ *
+ * @author      Magento Core Team <core@magentocommerce.com>
+ * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
+ */
+class Data extends \Magento\Framework\App\Helper\AbstractHelper
+{
+    private CustomerRepositoryInterface $customerRepository;
+
+    /**
+     * Data constructor.
+     *
+     * @param \Magento\Framework\App\Helper\Context $context
+     * @param CustomerRepositoryInterface $customerRepository
+     * @codeCoverageIgnore
+     */
+    public function __construct(
+        \Magento\Framework\App\Helper\Context $context,
+        CustomerRepositoryInterface $customerRepository
+    ) {
+        $this->customerRepository = $customerRepository;
+        parent::__construct($context);
+    }
+
+    /**
+     * Retrieve checkout session model
+     *
+     * @return string
+     * @codeCoverageIgnore
+     */
+    public function getCustomerName($customerId){
+        if($customerId != null){
+            $customer = $this->customerRepository->getById($customerId);
+            if($customer->getFirstname()){
+                return $customer->getFirstName()." ".$customer->getLastName();
+            }
+        }
+        return "";
+    }
+}
